29d1559a16a1c5bf5d36486ab41d5aad1a7a540d
Rubens Cavalheiro <rubens.cavalheiro@candeias.tur.br>
2018-03-06 15:01:34 -0300

    adicionado extensao de audio
    configuracao das expecificacoes de cada audio

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,20 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 	
-	var recordedAudioURL: URL!
-
     @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, highPitch, lowPitch, echo, reverb
+    }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("toca audio ao ser pressionado")
+		switch ButtonType(rawValue: sender.tag)! {
+		case .slow:
+			playSound(rate: 0.5)
+		case .fast:
+			playSound(rate: 1.5)
+		case .highPitch:
+			playSound(pitch: 1000)
+		case .lowPitch:
+			playSound(pitch: -1000)
+		case .echo:
+			playSound(echo: true)
+		case .reverb:
+			playSound(reverb: true)
+		}
+		configureUI(.playing)
+    }
+    @IBAction func stopSoundForButton(_ sender: Any) {
+        print("para audio ao ser pressionado")
+		stopAudio()
+    }
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-
-        // Do any additional setup after loading the view.
+        setupAudio()
     }
 
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		configureUI(.notPlaying)
+	}
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
